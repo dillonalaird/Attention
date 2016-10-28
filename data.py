7d8ecdf9984e25c424ed51e7cf861c820d963a74
Dillon Laird <dillonalaird@gmail.com>
2016-10-28 10:28:58 -0700

    added correct being/end of sentence tokens

--- data.py
+++ data.py
@@ -18,22 +18,22 @@ def read_vocabulary(data_path):
     return {w:i for i,w in enumerate(open(data_path).read().splitlines())}
 
 
-def data_iterator(data_path_input, data_path_output, in_vocab, out_vocab, max_size, batch_size):
-    with open(data_path_input, "rb") as f_in, open(data_path_output) as f_out:
+def data_iterator(source_data_path, target_data_path, source_vocab, target_vocab, max_size, batch_size):
+    with open(source_data_path, "rb") as f_in, open(target_data_path) as f_out:
         prev_batch = 0
         next_batch = 0
         data_in    = []
         data_out   = []
         for i, (lin, lout) in enumerate(zip(f_in, f_out)):
             if next_batch - prev_batch < batch_size:
-                in_text = [in_vocab[w] if w in in_vocab else in_vocab["<unk>"]
+                in_text = [source_vocab[w] if w in source_vocab else source_vocab["<unk>"]
                            for w in lin.replace("\n", "").split(" ")][:max_size][::-1]
-                out_text = [out_vocab[w] if w in out_vocab else out_vocab["<unk>"]
-                            for w in lout.replace("\n", " " + str(out_vocab["</s>"]))
+                out_text = [target_vocab[w] if w in target_vocab else target_vocab["<unk>"]
+                            for w in lout.replace("\n", " " + str(target_vocab["</s>"]))
                             .split(" ")][:max_size-1]
-                out_text = [out_vocab["<s>"]] + out_text
-                data_in.append(pre_pad(in_text, in_vocab["<pad>"], max_size))
-                data_out.append(post_pad(out_text, out_vocab["<pad>"], max_size))
+                out_text = [target_vocab["<s>"]] + out_text
+                data_in.append(pre_pad(in_text, source_vocab["<pad>"], max_size))
+                data_out.append(post_pad(out_text, target_vocab["<pad>"], max_size))
                 next_batch += 1
             else:
                 prev_batch = next_batch
